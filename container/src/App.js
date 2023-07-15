25704adba3763e13c87061baf765f00f24da374b
Serhii Nahornyi <serhii.nahornyi@zerto.com>
2023-07-15 21:48:06 +0300

    add invalidation step

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <>
-            <h1 align="center">Hi from container</h1>
+            <h1 align="center">Hi from container!!</h1>
             <hr />
             <MarketingApp />
         </>
